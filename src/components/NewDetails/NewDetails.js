e39bad53b71b63ad3b6cbe5db7cb26ca40363df2
lingfengxie <2801416900@qq.com>
2017-12-27 14:59:26 +0800

    ♨更新♨

--- src/components/NewDetails/NewDetails.js
+++ src/components/NewDetails/NewDetails.js
@@ -211,6 +211,14 @@ export default React.createClass({
                     <div className="bottomItem">
                         <HotRecom list={this.state.HotLists}></HotRecom>
                         <GuessLike list={this.state.GuessList}></GuessLike>
+                        <div className="botBut">
+                            <div className="message">
+                                <span className="title">
+                                    今日头条
+                                </span>
+                                <span className="desc">查看全部精彩评论</span>
+                            </div>
+                        </div>
                     </div>
                 </div>   
             </section>
