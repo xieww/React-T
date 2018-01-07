75aea78273b4e7ccabeceb1ad5db01faf6027a81
lingfengxie <2801416900@qq.com>
2018-01-07 10:57:08 +0800

    ♨更新♨

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -3,13 +3,13 @@ import util from '../core/Util';
 import { apiBaseUrl, isMobileClient } from './ApiConfig';
 
 const urlMap = {
-    'getNews': 'https://m.toutiao.com/list/?tag=__all__&ac=wap&count=20&format=json_raw&as=A1958AA43CAE16A&cp=5A4C6EF1B6EA1E1&min_behot_time=1514987245',
+    getNews: 'https://m.toutiao.com/list/?tag=__all__&ac=wap&count=20&format=json_raw&as=A1958AA43CAE16A&cp=5A4C6EF1B6EA1E1&min_behot_time=1514987245',
+    getNewsDetail: 'https://m.toutiao.com/i{{groupId}}/info/?_signature=P4l3zhAVZczD8J2yQcB56z-Jd9&i={{groupId}}'
 };
 
 const ApiService = {
     getNews(requestConfig, callback) {
         const url = urlMap.getNews;
-
         return http.get(url, requestConfig.params).then(response => {
             console.log(response);
             return callback(response);
