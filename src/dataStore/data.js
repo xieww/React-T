bd305b6daaf3a8138fa712aa4564f3ccf0a4b15f
lingfengxie <2801416900@qq.com>
2018-01-08 20:45:18 +0800

    ♨更新♨

--- src/dataStore/data.js
+++ src/dataStore/data.js
@@ -1,3 +1,38 @@
 export default {
-    
+    tags: [
+        {
+            tag: '_all_',
+            text: '推荐',
+        },{
+            tag: 'video',
+            text: '视频',
+        },{
+            tag: 'news_hot',
+            text: '热点',
+        },{
+            tag: 'news_society',
+            text: '社会',
+        },{
+            tag: 'news_entertainment',
+            text: '娱乐',
+        },{
+            tag: 'news_military',
+            text: '军事',
+        },{
+            tag: 'news_tech',
+            text: '科技',
+        },{
+            tag: 'news_sports',
+            text: '体育',
+        },{
+            tag: 'news_world',
+            text: '国际',
+        },{
+            tag: 'news_fashion',
+            text: '时尚',
+        },{
+            tag: 'news_finance',
+            text: '财经',
+        }
+    ]
 }
