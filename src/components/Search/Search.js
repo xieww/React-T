41293dd74a520ff0056bcfc42a8c33db1ae6cb9e
lingfengxie <2801416900@qq.com>
2018-01-10 18:25:33 +0800

    ♨更新♨

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -13,7 +13,35 @@ export default React.createClass({
                         <div className="abs_m">搜索</div>
                     </div>
                 </header>
-
+                <form className="search_form">
+                    <div className="search_content">
+                        <a href="#" className="search_a" />
+                        {/* <div className="arrowdown"></div> */}
+                        <input
+                            type="search"
+                            placeholder="请输入搜索关键词"
+                            id="search_input"
+                            name="keyword"
+                            value=""
+                        />
+                    </div>
+                </form>
+                <div className="hot_list">
+                    <div className="hot_words">
+                        <ul className="toutiao">
+                            <li>
+                                <a class="hot_words_link" href="">
+                                    黎明肖像被盗用
+                                </a>
+                            </li>
+                            <li>
+                                <a class="hot_words_link" href="">
+                                    女护士遇害案告破
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </section>
         );
     }
